66558867491f989e0a5083a546349f1199e59b25
bramiller1989 <bramiller1989@gmail.com>
2021-01-18 21:43:00 -0600

    string to byte using Convert.ToByte() and try/catch overflow exception

--- Program.cs
+++ Program.cs
@@ -22,8 +22,21 @@ namespace TypeConversion
 
             Console.WriteLine("Convert non compatible type - string to int using Convert.ToInt32()");
             var number = "1234";
-            int stringToInt = Convert.ToInt32(number);
-            Console.WriteLine(stringToInt);
+            i = Convert.ToInt32(number);
+            Console.WriteLine(i);
+
+            Console.WriteLine("-----------------------------------------------");
+
+            try
+            {
+                Console.WriteLine("Convert non compatible type - string to byte using Convert.ToByte() and try/catch overflow exception");
+                b = Convert.ToByte(number);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte");
+            }
         }
     }
 }
