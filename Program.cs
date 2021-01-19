59e0f5cb021a0cdac17024828606cc71aee8069a
bramiller1989 <bramiller1989@gmail.com>
2021-01-18 21:30:27 -0600

    use casting - int to byte

--- Program.cs
+++ Program.cs
@@ -6,10 +6,17 @@ namespace TypeConversion
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Implicit Conversion - byte to int");
             byte b = 1;
             int i = b;
-
             Console.WriteLine(i);
+
+            Console.WriteLine("-----------------------------------------------");
+
+            Console.WriteLine("Explicit Conversion using casting - int to byte");
+            i = 1;
+            b = (byte)i;
+            Console.WriteLine(b);
         }
     }
 }
